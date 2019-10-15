0a45074d989bd67e5b77891a39a40b5c7f13686c
sijuan-gt <874835281@qq.com>
2019-10-15 21:39:29 +0800

    第四版

--- js/register.js
+++ js/register.js
@@ -6,4 +6,42 @@ $(function(){
     $(".space2").on("mouseout", function() {
         $(".pic1").hide();
     })
+    $("btn3").on("click",function(){
+        $(".result").style=
+        `
+        bg=str;
+        
+        `
+    })
+    
 })
+function yanSeString(){
+    var arr=[];
+    for(var i=97;i<=122;i++){
+        arr.push(String.fromCharCode(i));
+    }
+    for(var i=0;i<10;i++){
+        arr.push(i);
+    }
+    var str="#";
+    for(var i=0;i<6;i++){
+        var index=parseInt(Math.random()*arr.length);
+        str+=arr[index];
+    }
+    return str;
+}
+function yanzhengma(){
+    var arr=[];
+    for(var i=97;i<=122;i++){
+        arr.push(String.fromCharCode(i));
+    }
+    for(var i=0;i<10;i++){
+        arr.push(i);
+    }
+    var str="";
+    for(var i=0;i<5;i++){
+        var index=parseInt(Math.random()*arr.length);
+        str+=arr[index];
+    }
+    return str;
+}
